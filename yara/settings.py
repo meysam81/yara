1e9c2d1edfd7a465086ba400aa40d3b6ee70a564
Meysam Azad <MeysamAzad81@yahoo.com>
2019-03-12 13:10:41 +0330

    add default DRF throttle to settings.py

--- yara/settings.py
+++ yara/settings.py
@@ -124,12 +124,20 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
 }
 
 # separate db for test
 if 'test' in sys.argv:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.test'
+        'NAME': 'db.test.sqlite3'
     }
