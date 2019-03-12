f25469617198c27c9452e131ffc5aea435fa1533
Meysam <MeysamAzad81@yahoo.com>
2019-03-12 11:30:47 +0330

    removed unnecessary id as DB creates it automatically

--- purchases/models.py
+++ purchases/models.py
@@ -5,7 +5,6 @@ from django.core.validators import validate_email, RegexValidator
 class Purchase(models.Model):
     purchase_date = models.DateTimeField(auto_now_add=True)
     purchase_name = models.CharField(max_length=150)
-    purchase_id = models.AutoField(name='id', primary_key=True)
     user_id = models.IntegerField()
     user_name = models.CharField(max_length=150)
     phone_number = RegexValidator(regex=r'^\+?1?\d{9,20}$',
