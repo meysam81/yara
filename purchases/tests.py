21a1c57daafb8a85021ab17f632c78fec735ef9f
Meysam <MeysamAzad81@yahoo.com>
2019-03-12 12:13:35 +0330

    write more tests

--- purchases/tests.py
+++ purchases/tests.py
@@ -4,18 +4,18 @@ from requests import post, put, delete, get
 from http import HTTPStatus
 
 
-class TestCrudPurchase(TestCase):
+class TestPurchase(TestCase):
     _NUMBER_OF_REQUESTS = 10
 
     def test_create_purchase(self):
 
         for i in range(self._NUMBER_OF_REQUESTS):
             p = {
-                "purchase_name": f"purchase{i}",
+                "purchase_name": "purchase",
                 "user_id": 100,
-                "user_name": "user1",
-                "phone_number": "+0987654321",
-                "email": "test1@example.com",
+                "username": "user1",
+                "phone_number": f"+098765432{i}",
+                "email": f"test{i}@example.com",
                 "address": "address1",
             }
 
@@ -50,8 +50,67 @@ class TestCrudPurchase(TestCase):
         results = loads(res.content).get('results', [])
         for i in results:
             purchase = i
-            purchase["user_name"] = f'UpdatedUserName{i["id"]}'
+            purchase["username"] = f'UpdatedUserName{i["id"]}'
 
             res = put(f'http://localhost:8000/purchases/{i["id"]}/', data=purchase)
             self.assertEqual(HTTPStatus.OK, res.status_code)
+
+    def test_create_purchase_with_empty_body(self):
+
+        p = {}
+        res = post('http://localhost:8000/purchases/')
+        self.assertEqual(HTTPStatus.BAD_REQUEST, res.status_code)
+
+        fields = [
+            'purchase_name',
+            'user_id',
+            'username',
+            'phone_number',
+            'email',
+            'address',
+        ]
+
+        result = loads(res.content)
+
+        error_message = 'This field is required.'
+        for k in result.keys():
+            self.assertIn(error_message, result[k])
+
+    def test_create_purchase_with_bad_email(self):
+
+        p = {
+            'purchase_name': 'product1',
+            'user_id': 'user1',
+            'username': 'username1',
+            'phone_number': '98912345678',
+            'email': 'this is a bad email',
+            'address': 'address1',
+        }
+
+        res = post('http://localhost:8000/purchases/', data=p)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, res.status_code)
+
+        result = loads(res.content)
+
+        error_message = 'Enter a valid email address.'
+        self.assertIn(error_message, result['email'])
+
+    def test_create_purchase_with_bad_phone_number(self):
+
+        p = {
+            'purchase_name': 'product1',
+            'user_id': 'user1',
+            'username': 'username1',
+            'phone_number': 'this is a bad phone number',
+            'email': 'test@example.com',
+            'address': 'address1',
+        }
+
+        res = post('http://localhost:8000/purchases/', data=p)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, res.status_code)
+
+        result = loads(res.content)
+
+        error_message = 'Enter a valid value.'
+        self.assertIn(error_message, result['phone_number'])
 
