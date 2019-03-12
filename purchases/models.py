a20b8c386e260953bb9d2dee517f3dd6a13247df
Meysam <MeysamAzad81@yahoo.com>
2019-03-12 12:12:56 +0330

    edit fields in models

--- purchases/models.py
+++ purchases/models.py
@@ -6,10 +6,10 @@ class Purchase(models.Model):
     purchase_date = models.DateTimeField(auto_now_add=True)
     purchase_name = models.CharField(max_length=150)
     user_id = models.IntegerField()
-    user_name = models.CharField(max_length=150)
-    phone_number = RegexValidator(regex=r'^\+?1?\d{9,20}$',
-                                  message="Phone number must be entered in the "
-                                          "format: '+999999999'. Up to 20 "
-                                          "digits allowed.",)
+    username = models.CharField(max_length=150)
+    phone_number = models.CharField(null=False, blank=False, max_length=20,
+                                    validators=[RegexValidator(
+                                        regex=r'^\+?1?\d{9,20}$'),
+                                    ])
     email = models.CharField(max_length=150, validators=[validate_email])
     address = models.TextField()
